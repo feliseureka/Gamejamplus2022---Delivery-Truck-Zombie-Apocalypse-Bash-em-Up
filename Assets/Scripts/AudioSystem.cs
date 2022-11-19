75d7f7df27b35d44d4bf74fb69f280d166d4640c
feliseureka <feliseureka@gmail.com>
2022-11-19 18:57:09 +0700

    Added AudioSystem

---
+++ Assets/Scripts/AudioSystem.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class AudioSystem : MonoBehaviour
+{
+    public static AudioSystem Instance;
+    public AudioSource musicSource, SFXsource;
+
+    public AudioClip[] MusicArray;
+    public AudioClip[] SFXArray;
+
+    void Start(){
+        if(Instance != null && Instance != this){
+            Destroy(this);
+        }else{
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void PlayMusic(int i){
+        AudioClip s = MusicArray[i];
+        musicSource.clip = s;
+        musicSource.Play();
+    }
+
+    public void PlaySFX(int i){
+        AudioClip s = SFXArray[i];
+        SFXsource.clip = s;
+        SFXsource.Play();
+    }
+
+    public void ChangeMusicVolume(float value){
+        musicSource.volume = value;
+    }
+
+    public void ChangeSFXVolume(float value){
+        SFXsource.volume = value;
+    }
+
+    public void StopMusic(){
+        musicSource.Stop();
+    }
+
+    public void StopSFX(){
+        SFXsource.Stop();
+    }
+}
