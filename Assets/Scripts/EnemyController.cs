562743fe6a22779432b21d7926cec8b6d4dde0cc
Akemisora <akemisora040503@gmail.com>
2022-11-19 13:16:33 +0700

    VERY simple enemy control

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,12 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour
-{
-    Transform player;
+public class EnemyController : MonoBehaviour {
 
-    private void Start()
-    {
-        player = GetComponent<Transform>();
+    private SphereCollider detectionArea;
+    private Transform player;
+    private Rigidbody rb;
+
+    [SerializeField] private float speed;
+
+
+    private void Awake() {
+        detectionArea = GetComponent<SphereCollider>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate() {
+        if (!player || Time.frameCount % 16 == 0) { return; }
+        rb.velocity = speed * (player.position - transform.position);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            player = other.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.CompareTag("Player")) {
+            player = null;
+        }
     }
 }
