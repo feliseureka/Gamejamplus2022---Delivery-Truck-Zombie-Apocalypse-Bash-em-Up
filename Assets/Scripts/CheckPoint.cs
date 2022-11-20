bb0990f723439f6984f067828c4ee9075b000693
Akemisora <akemisora040503@gmail.com>
2022-11-20 12:03:09 +0700

    Sphere Checkpoint

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -6,7 +6,10 @@ public class CheckPoint : MonoBehaviour
 {
     [SerializeField] float spawnArea_height = 1f;
     [SerializeField] float spawnArea_width = 1f;
+    [SerializeField] float waitTime = 2.5f;
     public GameObject checkPoint;
+
+    private Coroutine waitPark;
 
     public void Spawn(){
         Vector3 rsp = new Vector3(Random.Range(-spawnArea_width, spawnArea_width), 1, Random.Range(-spawnArea_height, spawnArea_height));
@@ -15,10 +18,22 @@ public class CheckPoint : MonoBehaviour
     }
 
     void OnTriggerEnter(Collider other){
-        if(other.tag == "Player"){
-            other.transform.GetComponent<PlayerStats>().recoverHealth();
-            Spawn();
-            Destroy(gameObject);
+        if(other.CompareTag("Player")) {
+            waitPark = StartCoroutine(Wait(other));
         }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if (other.CompareTag("Player") && waitPark != null) {
+            StopCoroutine(waitPark);
+        }
+    }
+
+    IEnumerator Wait(Collider other) {
+        yield return new WaitForSeconds(waitTime);
+        other.transform.GetComponent<PlayerStats>().recoverHealth();
+        Spawn();
+        Destroy(gameObject);
+        waitPark = null;
     }
 }
